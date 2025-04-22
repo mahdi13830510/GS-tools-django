e02626fa7ff5d3f0fabc8c2fa23543ffebfa09a4
mahdi13830510 <mahdi.dlt13830510@Gmail.com>
2025-04-23 02:58:02 +0330

    feat(core): add `FileSizeValidator` to validators

---
+++ gs_tools_django/core/validators.py
@@ -0,0 +1,37 @@
+import re
+
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
+from django.utils.translation import gettext_lazy as _
+
+
+@deconstructible
+class FileSizeValidator:
+    """Validator for file size."""
+
+    def __init__(self, max_size: int, min_size: int = 0):
+        """Create a validator for file size.
+
+        Args:
+            max_size (int): max size in megabytes
+            min_size (int): min size in megabytes
+        """
+        self.max_size = max_size * 1000000
+        self.min_size = min_size * 1000000
+
+    def __call__(self, file):
+        if not self.min_size <= file.size <= self.max_size:
+            message = _("File size must be between %(min_size)s and %(max_size)s")
+            raise ValidationError(message % self.min_size % self.max_size)
+
+
+def is_valid_throttle_rate(rate):
+    # Regular expression to match the throttle rate pattern
+    pattern = re.compile(r"^(\d+)/((s|sec|m|min|h|hour|d|day))$")
+
+    # Check if the rate matches the pattern
+    if not pattern.match(rate):
+        msg = f"Incorrect throttling rate value: {rate}"
+        raise Exception(msg)
+
+    return rate
